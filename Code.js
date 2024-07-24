56e6e1c5d81cc6c9f62575d1139a6f869df03683
Igor-Felipe05 <igorfelipe9263@gmail.com>
2024-07-24 12:13:26 -0300

    Primeira Aula de Programação.

---
+++ Code.js
@@ -0,0 +1,3 @@
+let info2 = " 20 pães"
+let info = "Boa noite, essa noite todos estão convidados para o baile de máscaras, somente as pessoas constadas como vip, vão poder adentrar a área especial com bebidas exclusivas."
+console.log(info + info2)
